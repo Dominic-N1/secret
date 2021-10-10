3e5cd58dd05af78388a815646237878fee33884a
Dominic <vladimirndsm@gmail.com>
2021-10-10 16:10:55 +0300

    fourth level

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const sha512 = require('js-sha512');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
 
+const saltRounds = 10;
 const app = express();
 const port = 3000;
 
@@ -14,7 +14,9 @@ const port = 3000;
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 mongoose.connect('mongodb://localhost:27017/userDB');
 
@@ -23,8 +25,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 const User = new mongoose.model("User", userSchema)
 
 app.get("/", (req, res) => {
@@ -32,7 +32,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  res.render("login", {errMsg: "", username: "", password: ""});
+  res.render("login", {
+    errMsg: "",
+    username: "",
+    password: ""
+  });
 });
 
 app.get("/register", (req, res) => {
@@ -40,41 +44,58 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: sha512(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
 
-  newUser.save((err) => {
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = sha512(req.body.password);
+  const password = req.body.password;
 
-  User.findOne({email: username}, (err, foundUser) => {
+  User.findOne({
+    email: username
+  }, (err, foundUser) => {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-          console.log(`New login (${username})`);
-        } else {
-          res.render("login", {errMsg: "Email or password incorrect", username: username, password: password});
-        }
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            res.render("login", {
+              errMsg: "Email or password incorrect",
+              username: username,
+              password: password
+            });
+          }
+
+        })
+        console.log(`New login (${username})`);
       } else {
-        res.render("login", {errMsg: "Email or password incorrect", username: username, password: password});
+        res.render("login", {
+          errMsg: "Email or password incorrect",
+          username: username,
+          password: password
+        });
       }
     }
+
   });
 });
+
 
 app.listen(port, () => {
   console.log(`Server has started at http://localhost:${port}`);
